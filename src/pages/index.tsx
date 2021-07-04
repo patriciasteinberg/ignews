45717d4b61a948afb78da78acc573a9454e228b2
Patricia Steinberg <50302759+patriciasteinberg@users.noreply.github.com>
2021-07-03 22:37:05 -0400

    getting prices from Stripe API and GetStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,57 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import AvatarImg from "../../public/images/avatar.svg";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title>ig.news | Home</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount}/month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <Image src={AvatarImg} alt="Girl coding" />
+      </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1J9KL2Ev5w6Gus76ZwiBYz7K");
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount &&    
+    new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD"
+    }).format(price.unit_amount / 100)
+  };
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24 hours
+  }
 }
